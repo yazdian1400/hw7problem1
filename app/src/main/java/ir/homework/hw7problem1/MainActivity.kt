eb4f6a7f2fea7497312356ac264ddfe134871686
Maedeh <yazdian.oul@gmail.com>
2022-02-21 14:07:13 +0330

    define doCalculations function

--- app/src/main/java/ir/homework/hw7problem1/MainActivity.kt
+++ app/src/main/java/ir/homework/hw7problem1/MainActivity.kt
@@ -18,9 +18,45 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         digitList = mutableListOf("1", "0", ".", "5", "1")
-        Toast.makeText(this,"${toNumber(digitList)} ${digitList.size}",Toast.LENGTH_LONG).show()
+        numberList = mutableListOf(3.0, 2.0, 5.0)
+        signList = mutableListOf('/', '-')
+
+        Toast.makeText(this,"${doCalculations(numberList, signList)} ${signList.size}",Toast.LENGTH_LONG).show()
     }
     fun toNumber(digitList: MutableList<String>): Double{
        return digitList.reduce{str1: String, str2: String -> str1 + str2}.toDouble()
+    }
+
+    fun doCalculations(numberList: MutableList<Double>, signList: MutableList<Char>): Double{
+        var index: Int
+        while(signList.size != 0){
+            if (signList.contains('*')) {
+                index = signList.lastIndexOf('*')
+                numberList[index] = numberList[index] * numberList[index + 1]
+                numberList.removeAt(index + 1)
+                signList.removeAt(index)
+                continue
+            }
+            else if (signList.contains('/')) {
+                index = signList.lastIndexOf('/')
+                numberList[index] = numberList[index] / numberList[index + 1]
+                numberList.removeAt(index + 1)
+                signList.removeAt(index)
+                continue
+            }
+            else if (signList.contains('+')) {
+                index = signList.lastIndexOf('+')
+                numberList[index] = numberList[index] + numberList[index + 1]
+                numberList.removeAt(index + 1)
+                signList.removeAt(index)
+            }
+            else if (signList.contains('-')) {
+                index = signList.lastIndexOf('-')
+                numberList[index] = numberList[index] - numberList[index + 1]
+                numberList.removeAt(index + 1)
+                signList.removeAt(index)
+            }
+        }
+        return numberList.first()
     }
 }
